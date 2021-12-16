1d1a553a4c1aa8b426cb90ac4f51d151efc1d7fe
Manas Laha <manas080199@gmail.com>
2021-12-16 20:27:29 +0530

    Initial commit

---
+++ servicea/src/main/java/com/photon/servicea/ServiceaApplication.java
@@ -0,0 +1,15 @@
+package com.photon.servicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@RefreshScope
+public class ServiceaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceaApplication.class, args);
+	}
+
+}
